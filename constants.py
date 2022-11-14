5b5e7eedbc534e2c62068745f608910a4bd81f85
Sanzhar <48170101+SanzharMrz@users.noreply.github.com>
2022-11-14 19:30:14 +0600

    init - create constants

---
+++ constants.py
@@ -0,0 +1,29 @@
+first  = {0: 'another_intent', 
+          1: 'tracking_information'}
+
+second = {0: 'another_intent', 
+          1: 'other'}
+
+third =  {0: 'retailer_updates',
+          1: 'order_cancellation',
+          2: 'product_question',
+          3: 'refund',
+          4: 'address_changed',
+          5: 'damaged',
+          6: 'wrong_item_received',
+          7: 'missing_products',
+          8: 'promo_codes',
+          9: 'gift',
+          10: 'out_of_stock',
+          11: 'unsubscribe_to_mailing_list'}
+
+fourth_a = {0: 'product_question',
+            1: 'where_do_you_ship',
+            2: 'engraving',
+            3: 'detail_correction'}
+
+fourth_b = {0: 'refund', 
+            1: 'payment_issue'}
+
+fourth_c = {0: 'order_cancellation', 
+            1: 'change_order'}
